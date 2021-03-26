c5d3265d5e805237a0ccb2cfbe4dc8f88a7af172
mickvav <mickvav@server01.example.com>
2021-03-26 23:43:21 +0000

    Black

--- server/tests/unit.py
+++ server/tests/unit.py
@@ -18,25 +18,19 @@ class TestApp(unittest.TestCase):
     def test_normal_flow(self):
         response = self.app.get("/join", headers={"Content-Type": "text/html"})
         self.assertEqual(response.data, b"problems")
-        response = self.app.get("/join", 
-                headers={"Content-Type": "text/html"}, 
-                query_string={"id":self.client_id})
+        response = self.app.get(
+            "/join",
+            headers={"Content-Type": "text/html"},
+            query_string={"id": self.client_id},
+        )
         self.assertGreater(int(response.data), 100000)
         self.assertTrue(path.isdir(self.client_path))
-        response = self.app.get("/lastfile", 
-                headers={"Content-Type": "text/html"}, 
-                query_string={"id":self.client_id})
+        response = self.app.get(
+            "/lastfile",
+            headers={"Content-Type": "text/html"},
+            query_string={"id": self.client_id},
+        )
         self.assertEqual(response.data, b"0.jpg")
-        data = { "file": (io.BytesIO(b"aaaaaaa"), "123.jpg") , "id": self.client_id}
+        data = {"file": (io.BytesIO(b"aaaaaaa"), "123.jpg"), "id": self.client_id}
         response = self.app.post("/put", data=data, content_type="multipart/form-data")
         self.assertEqual(response.data, b"done")
-
-
-
-
-        
-
-
-
-
-    # curl http://127..0.1:5000/join?id=1
